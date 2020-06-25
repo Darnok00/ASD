1b17f38a86b3537e1fea8f41e6a1c5a007bcbe37
Darnok00 <56363711+Darnok00@users.noreply.github.com>
2020-06-25 14:35:02 +0200

    Create SelectionSort.py

---
+++ Sortowanie/SelectionSort.py
@@ -0,0 +1,15 @@
+#klasyczny algorytm sortowania przez wybieranie 
+def selection_sort (tab,n):
+        for i in range (0,n-1):
+            minimum = i
+            for j in range(i+1,n):
+                if tab[j] < tab[minimum]:
+                    minimum = j
+            tab[minimum], tab[i] = tab[i], tab[minimum]
+n = int(input("Podaj ilosc liczb ziom: "))
+tab = [0]*n
+for i in range(0,n):
+    tab[i]=int(input())
+selection_sort(tab,n)
+for i in range(0,n):
+    print(tab[i])
